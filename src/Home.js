4b4b493c048f177fb45b6a237ea287d1771139de
karim322 <117765755+karim322@users.noreply.github.com>
2024-01-23 21:26:02 +0200

    fsfsf

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
-    <div>this is Home 2</div>
+    <div>this is Home 2
+
+      <Link to='/about' >about</Link>
+      <Link to='/param/12345' >param</Link>
+    </div>
   )
 }
 
