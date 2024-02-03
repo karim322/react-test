fb676db62abde482f8f4fe1367019d1401c9159c
karim322 <117765755+karim322@users.noreply.github.com>
2024-02-04 00:41:53 +0200

    home update

---
+++ index.js
@@ -0,0 +1,28 @@
+/*let heroInterval;
+window.onload=()=>{
+  let heroDiv =1;
+  const heroArray = Array.from(document.querySelectorAll('#hero-scroll-container > div'));
+  heroInterval = setInterval(() => {
+    if(heroDiv<heroArray.length){
+      heroArray[heroDiv].style.zIndex=heroDiv;
+      heroArray[heroDiv-1].classList.add('prevVisibileDiv');
+      heroArray[heroDiv].classList.add('visibleDiv');
+      heroDiv++;
+    }else{
+      heroArray.map((e)=>{
+        e.classList.remove('visibleDiv');
+        e.classList.remove('prevVisibileDiv');
+      })
+      heroDiv = 1;
+    }
+  }, 8000);
+  window.onscroll=()=>{
+  if(document.querySelector('.siteGlimpse').children.div1.getBoundingClientRect().top<window.innerHeight-100){
+    Array.from(document.querySelector('.siteGlimpse').children).map((e)=>e.classList.add('ontoViewDiv'));
+  };
+}
+}
+window.onabort=()=>{
+  clearInterval(heroInterval)
+}*/
+
